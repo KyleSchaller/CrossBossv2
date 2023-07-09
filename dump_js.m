a428c3281d945eaffa975ef03c689e0c1259441f
Kyle <schaller.k@gmail.com>
2023-07-08 19:07:52 -0700

    Crossbossv2
    Added level 8 and fixed an error in the cost function.

---
+++ dump_js.m
@@ -0,0 +1,7 @@
+%
+
+X = Data.X_points.*40;
+Y = 10-Data.Y_points;
+Y = Y.*40;
+
+text_str = ['[[' num2str(X(1)) ',' num2str(Y(1)) '],[' num2str(X(2)) ',' num2str(Y(2)) '],[' num2str(X(3)) ',' num2str(Y(3)) '],[' num2str(X(4)) ',' num2str(Y(4)) '],[' num2str(X(5)) ',' num2str(Y(5)) '],[' num2str(X(6)) ',' num2str(Y(6)) ']]'] 
